239ac44118ba2e0b4154cfd7b41f26dbb521b6bd
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-11-01 21:51:37 +0700

    Update donasi.cjs

--- plugins/donasi.cjs
+++ plugins/donasi.cjs
@@ -1,14 +1,14 @@
 var handler = async m => m.reply(`
 ╭─「 Donasi 」
-│ • TELKOMSEL [08211499086]
-│ • DANA [08211499086]
-│ • OVO [08211499086]
-│ • GOPAY [08211499086]
-│ • SHOPPY PAY [08211499086]
+│ • TELKOMSEL [6281368176386]
+│ • DANA [6281368176386]
+│ • OVO [6281368176386]
+│ • GOPAY [6281368176386]
+│ • SHOPPY PAY [6281368176386]
 │ • Saweria [https://saweria.co/botstyle]
 ╰────
 ╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/62895368900456
+│ > Ingin donasi? Wa.me/6281368176386
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
